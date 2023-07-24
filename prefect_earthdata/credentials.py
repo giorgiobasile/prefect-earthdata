13f437137498877f1e2d6e7ffaee43ce9e376737
Giorgio Basile <giorgiobasile@users.noreply.github.com>
2023-07-24 21:47:30 +0200

    Fix credentials doc URL and add Earthdata login link

--- prefect_earthdata/credentials.py
+++ prefect_earthdata/credentials.py
@@ -10,7 +10,10 @@ from pydantic import Field, SecretStr
 class EarthdataCredentials(Block):
     """
     Block used to manage authentication with NASA Earthdata.
-    NASA Earthdata authentication is handled via the `earthaccess` module.
+    To obtain an account, please refer to the
+    [Earthdata Login docs](https://www.earthdata.nasa.gov/eosdis/science-system-description/eosdis-components/earthdata-login).
+
+    Authentication is handled via the `earthaccess` module.
     Refer to the [earthaccess docs](https://nsidc.github.io/earthaccess/)
     for more info about the possible credential configurations.
 
@@ -25,7 +28,7 @@ class EarthdataCredentials(Block):
 
     _logo_url = "https://yt3.googleusercontent.com/ytc/AGIKgqPjIUeAw3_hrkHWZgixdwD5jc-hTWweoCA6bJMhUg=s176-c-k-c0x00ffffff-no-rj"  # noqa
     _block_type_name = "NASA Earthdata Credentials"
-    _documentation_url = "https://nsidc.github.io/earthaccess/"  # noqa
+    _documentation_url = "https://giorgiobasile.github.io/prefect-earthdata/credentials/#prefect_earthdata.credentials.EarthdataCredentials"  # noqa
 
     earthdata_username: str = Field(
         default=...,
